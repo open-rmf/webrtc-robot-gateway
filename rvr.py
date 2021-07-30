0c273bf4398a065b928d73c138f66a2bbe27891d
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-07-30 12:19:32 +0800

    update
    Signed-off-by: Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>

--- rvr.py
+++ rvr.py
@@ -1,32 +1,34 @@
-import sys
 import os
-import time
-from pathlib import Path
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 
 import asyncio
-
-from sphero_sdk import SerialAsyncDal
 from sphero_sdk import SpheroRvrAsync
+from sphero_sdk import SerialAsyncDal
 from sphero_sdk import RawMotorModesEnum
+from pathlib import Path
+import time
 
-# initialize global variables
-speed = 75
-rot_speed = 115
 
 loop = asyncio.get_event_loop()
+
 rvr = SpheroRvrAsync(
-        dal=SerialAsyncDal(
-            loop
-            )
+    dal=SerialAsyncDal(
+        loop
         )
+    )
+
+speed = 75
+rot_speed = 75
 
 
 async def main():
-    global speed
-    global heading
-    global flags
+    """ This program has RVR drive around in different directions.
+    """
 
     await rvr.wake()
+
+    await asyncio.sleep(2)
 
     await rvr.reset_yaw()
 
@@ -42,61 +44,65 @@ async def main():
 
         teleop = line.rstrip()
 
-        if teleop == 'W':  # W
-            print('W')
+        print(teleop)
+        if teleop == 'W':
             await rvr.raw_motors(
-                    left_mode=RawMotorModesEnum.forward.value,
-                    left_duty_cycle=speed,  # Valid duty cycle range is 0-255
-                    right_mode=RawMotorModesEnum.forward.value,
-                    right_duty_cycle=speed  # Valid duty cycle range is 0-255
-                    )
-        elif teleop == 'A':  # A
-            print('A')
+                left_mode=RawMotorModesEnum.forward.value,
+                left_speed=speed,  # Valid speed values are 0-255
+                right_mode=RawMotorModesEnum.forward.value,
+                right_speed=speed  # Valid speed values are 0-255
+                )
+
+        elif teleop == 'S':
             await rvr.raw_motors(
-                    left_mode=RawMotorModesEnum.reverse.value,
-                    left_duty_cycle=rot_speed,  # Valid duty cycle range is 0-255
-                    right_mode=RawMotorModesEnum.forward.value,
-                    right_duty_cycle=rot_speed  # Valid duty cycle range is 0-255
-                    )
-        elif teleop == 'S':  # S
-            print('S')
+                left_mode=RawMotorModesEnum.reverse.value,
+                left_speed=speed,  # Valid speed values are 0-255
+                right_mode=RawMotorModesEnum.reverse.value,
+                right_speed=speed  # Valid speed values are 0-255
+                )
+
+        elif teleop == 'A':
             await rvr.raw_motors(
-                    left_mode=RawMotorModesEnum.reverse.value,
-                    left_duty_cycle=speed,  # Valid duty cycle range is 0-255
-                    right_mode=RawMotorModesEnum.reverse.value,
-                    right_duty_cycle=speed  # Valid duty cycle range is 0-255
-                    )
-        elif teleop == 'D':  # D
-            print('D')
+                left_mode=RawMotorModesEnum.reverse.value,
+                left_speed=rot_speed,  # Valid speed values are 0-255
+                right_mode=RawMotorModesEnum.forward.value,
+                right_speed=rot_speed  # Valid speed values are 0-255
+                )
+
+        elif teleop == 'D':
             await rvr.raw_motors(
-                    left_mode=RawMotorModesEnum.forward.value,
-                    left_duty_cycle=rot_speed,  # Valid duty cycle range is 0-255
-                    right_mode=RawMotorModesEnum.reverse.value,
-                    right_duty_cycle=rot_speed  # Valid duty cycle range is 0-255
-                    )
-        elif teleop == 'X':  # SPACE
-            print('X')
+                left_mode=RawMotorModesEnum.forward.value,
+                left_speed=rot_speed,  # Valid speed values are 0-255
+                right_mode=RawMotorModesEnum.reverse.value,
+                right_speed=rot_speed  # Valid speed values are 0-255
+                )
+
+        elif teleop == 'X':
             await rvr.raw_motors(
-                    left_mode=RawMotorModesEnum.forward.value,
-                    left_duty_cycle=0,  # Valid duty cycle range is 0-255
-                    right_mode=RawMotorModesEnum.forward.value,
-                    right_duty_cycle=0  # Valid duty cycle range is 0-255
-                    )
+                left_mode=RawMotorModesEnum.forward.value,
+                left_speed=0,  # Valid speed values are 0-255
+                right_mode=RawMotorModesEnum.forward.value,
+                right_speed=0  # Valid speed values are 0-255
+                )
+        else:
+            continue
 
-        # sleep the infinite loop for a 10th of a second to avoid flooding the serial port.
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0.5)
 
-
-def run_loop():
-    global loop
-    loop.run_until_complete( asyncio.gather(main()))
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
-        run_loop()
+        loop.run_until_complete(
+            main()
+            )
+
     except KeyboardInterrupt:
-        print("Keyboard Interrupt...")
+        print('\nProgram terminated with keyboard interrupt.')
+
+        loop.run_until_complete(
+            rvr.close()
+            )
+
     finally:
-        print("Press any key to exit.")
-        exit(1)
+        if loop.is_running():
+            loop.close()
+
